e3524473203a0e5d7c5caeec049132e33dfd3b41
kystudio <yuan012308@hotmail.com>
2016-11-16 09:47:53 +0800

    完善功能

--- app/src/main/java/com/kystudio/learnservice/MainActivity.java
+++ app/src/main/java/com/kystudio/learnservice/MainActivity.java
@@ -4,13 +4,21 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, ServiceConnection {
+    private TextView tvOut;
+    private EditText etData;
     private Intent intent;
+    private MyService.Binder binder = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +27,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         intent = new Intent(MainActivity.this, MyService.class);
 
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        etData = (EditText) findViewById(R.id.etData);
+
         findViewById(R.id.btnStartService).setOnClickListener(this);
         findViewById(R.id.btnStopService).setOnClickListener(this);
         findViewById(R.id.btnBindService).setOnClickListener(this);
         findViewById(R.id.btnUnBindService).setOnClickListener(this);
+        findViewById(R.id.btnSyncData).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btnStartService:
+                intent.putExtra("data",etData.getText().toString());
                 startService(intent);
                 break;
             case R.id.btnStopService:
@@ -38,8 +51,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 bindService(intent, this, Context.BIND_AUTO_CREATE);
                 break;
             case R.id.btnUnBindService:
-                System.out.println("unbind");
                 unbindService(this);
+                break;
+            case R.id.btnSyncData:
+                if (binder != null) {
+                    binder.setData(etData.getText().toString());
+                }
                 break;
         }
     }
@@ -47,10 +64,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
         System.out.println("Service conntected!");
+        binder = (MyService.Binder) iBinder;
+        binder.getService().setCallback(new MyService.Callback(){
+            @Override
+            public void onDataChange(String data) {
+                Message msg = new Message();
+                Bundle b = new Bundle();
+                b.putString("data",data);
+                msg.setData(b);
+                handler.sendMessage(msg);
+            }
+        });
     }
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            tvOut.setText(msg.getData().getString("data"));
+        }
+    };
 
     @Override
     public void onServiceDisconnected(ComponentName componentName) {
+        // 异常崩溃退出的时候调用
         System.out.println("Service disconntected!");
     }
 }
--- app/src/main/java/com/kystudio/learnservice/MyService.java
+++ app/src/main/java/com/kystudio/learnservice/MyService.java
@@ -2,13 +2,23 @@ package com.kystudio.learnservice;
 
 import android.app.Service;
 import android.content.Intent;
-import android.os.Binder;
 import android.os.IBinder;
 
 public class MyService extends Service {
-    private boolean serviceRunning;
+    private boolean serviceRunning = false;
+    private String data = "这是默认信息";
 
     public MyService() {
+    }
+
+    public class Binder extends android.os.Binder {
+        public void setData(String data) {
+            MyService.this.data = data;
+        }
+
+        public MyService getService(){
+            return MyService.this;
+        }
     }
 
     @Override
@@ -21,17 +31,22 @@ public class MyService extends Service {
         super.onCreate();
         serviceRunning = true;
         System.out.println("service create");
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        new Thread(){
+        new Thread() {
             @Override
             public void run() {
                 super.run();
 
+                int i = 0;
+
                 while (serviceRunning) {
-                    System.out.println("服务正在运行...");
+                    i++;
+                    String str = i + ":" + data;
+                    System.out.println(str);
+
+                    if (callback!=null){
+                        callback.onDataChange(str);
+                    }
+
                     try {
                         sleep(1000);
                     } catch (InterruptedException e) {
@@ -40,8 +55,36 @@ public class MyService extends Service {
                 }
             }
         }.start();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        System.out.println("service startcommand");
+//        new Thread() {
+//            @Override
+//            public void run() {
+//                super.run();
+//
+//                while (serviceRunning) {
+//                    System.out.println("服务正在运行...");
+//                    try {
+//                        sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//        }.start();
+        data = intent.getStringExtra("data");
 
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        System.out.println("service Unbind");
+        serviceRunning = false;
+        return super.onUnbind(intent);
     }
 
     @Override
@@ -49,5 +92,19 @@ public class MyService extends Service {
         super.onDestroy();
         System.out.println("service destory");
         serviceRunning = false;
+    }
+
+    private Callback callback = null;
+
+    public Callback getCallback() {
+        return callback;
+    }
+
+    public void setCallback(Callback callback) {
+        this.callback = callback;
+    }
+
+    public static interface Callback{
+        void onDataChange(String data);
     }
 }
